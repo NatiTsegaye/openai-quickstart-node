cf318a659c0bbe2f385d537774141dcc13a47534
Nathnael Hailu Tsegaye <nht249@nyu.edu>
2023-01-04 03:53:29 +0400

    200 max_tokens

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -65,7 +65,7 @@ const getSummary = async (transcript) => {
     model: "text-davinci-003",
     prompt: `summarize the following in a first person voice and concise paragraphs: ${transcript}}`,
     temperature: 0.6,
-    max_tokens: 500,
+    max_tokens: 200,
   });
   return response.data.choices[0].text;
 };
